3c2004c2fa5674dc9a284273ff1e5541ca87624d
John Kimanzi <doctorjohnkey.js@gmail.com>
2022-01-21 10:37:07 +0300

    Add files via upload

---
+++ about.php
@@ -0,0 +1,44 @@
+
+
+    <!-- Navigation -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header" align="center"><font color="#00FF33">DIGISCHOOL FEEDBACK SYSTEM</font> </h1>
+                
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Intro Content -->
+        <div class="row" style="margin-bottom:50px;margin-left:50px">
+           
+               <P> 
+The Government of Kenya through the Ministry of Education in 2013 started the Digital Literacy Program meant to integrate technology in teaching and learning in basic education system. The programme had five projects components each requiring
+agency mandated with the project component to be involved for successful implementation. The programme failed to take off largely due to its design and leadership approach which did not facilitate extensive consultation and involvement of agencies 
+mandated with key project components. The government on noticing the challenge ,in 2015, a new approach was adopted where each agency mandated with a project component was allocated the project with a policy guide to facilitate implementation. 
+The government, through a cabinet memo, restructured the programme as follows; Ministry of Education to provide policy guidelines for the project, KICD to develop content, TSC to train teachers , Kenya Power and REA to electrify schools , 
+Ministry of industrialization to ensure set up local assembly plants and ICTA to provide learning devices to schools and coordinate implementation. This new approach enable the programme to be implemented with minimum challenges.
+
+Kenya has endorsed the Millennium Development Goals (MDG) for 2015 which were ratified by the United Nations. The second MDG targets the achievement of universal primary education, which Kenya has achieved. However, 
+the spirit of MDG 2 goes beyond merely the attendance of children at primary school to issues such as quality, ensuring that children attain the right skills and competencies at the primary level of education. Additionally, 
+Kenya subscribed to Sustainable Development Goal (SDG) which further emphasized inclusive and equitable education and promoted lifelong learning opportunities for all.
+
+To provide a rich, digital environment, the government has undertaken to integrate ICT across all levels of education through the Digital Literacy Programme (DLP). The Programme is aimed at imparting the required competencies for 21st century economy. 
+This programme is being implemented in three phases whereby Phase I targets young learners of grade 1 to 3, phase II targets learners of grade 4-6 and phase III targets advanced learners of grade 7 and above.
+
+The roll out of phase I is complete and the government is now in the process of implementing phase II in the next three years beginning 2019 to 2022. The programme is fully integrated with Competency Based curriculum [CBC] 
+which the government is currently rolling out under the Ministry of Education.
+
+This next phase recommends full integration of the programme with CBC, setup of smart classrooms through improvement of the current facilities, provision of variety of learning materials in the classrooms among them, 
+advanced digital devices, programmable output devices, projectors, online updated contents, provision of affordable electricity to school, training of teachers, provision of internet connectivity to schools, adequate technical support and stakeholder engagement.
+
+The management of the programme will be through the multi-agency approach where ICT Authority remains the key implementer and provision of appropriate technologies, Teacher Service Commission [TSC] conduct training for teachers, 
+Kenya Institute of Curriculum Development [KICD] provide required content, Ministry of Energy to supply electricity to schools, Ministry of Education to develop required school infrastructure and Ministry of Industrialization to ensure the use of local content
+</P> 
+               
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Footer -->
+     
---
+++ login.php
@@ -0,0 +1,71 @@
+<?php 
+extract($_POST);
+if(isset($save))
+{
+
+	if($n=="" || $pass=="")
+	{
+	$err="<font color='red'>fill all the fileds first</font>";	
+	}
+	else
+	{
+$pass=md5($pass);	
+
+$sql=mysqli_query($conn,"select * from user where name='$n' and pass='$pass'");
+
+$r=mysqli_num_rows($sql);
+
+if($r==true)
+{
+$_SESSION['user']=$n;
+header('location:user');
+}
+
+else
+{
+
+$err="<font color='red'>Invalid login details</font>";
+
+}
+}
+}
+
+?>
+<div class="row">
+		<div class="col-sm-2"></div>
+		<div class="col-sm-8">
+
+<form method="post">
+	<div class="row">
+		<div class="col-sm-4"></div>
+		<div class="col-sm-4"><h2>Login Form</h2></div>
+	</div>
+	
+	<div class="row">
+		<div class="col-sm-4"></div>
+		<div class="col-sm-4"><?php echo @$err;?></div>
+	</div>
+	
+	
+	
+	<div class="row" style="margin-top:10px">
+		<div class="col-sm-4">Enter Your Name</div>
+		<div class="col-sm-5">
+		<input type="name" name="n" class="form-control"/></div>
+	</div>
+	
+	<div class="row" style="margin-top:10px">
+		<div class="col-sm-4">Enter YOur Password</div>
+		<div class="col-sm-5">
+		<input type="password" name="pass" class="form-control"/></div>
+	</div>
+	<div class="row" style="margin-top:10px">
+		<div class="col-sm-4"></div>
+		<div class="col-sm-8">
+		<input type="submit" value="Login" name="save" class="btn btn-info"/>
+		
+		</div>
+	</div>
+</form>	
+</div>
+</div>
